3c99a590f6dbb8d21830c9d49e5af4d91551bda4
afarukcali <afarukcali@gmail.com>
2023-08-25 17:37:04 +0300

    vesting

--- vesting2.js
+++ vesting2.js
@@ -1,26 +1,28 @@
 const fs = require("fs");
-const { RuntimeArgs, CLValueBuilder, Contracts, CasperClient, Keys, CLPublicKey, CLURef, Signer, CasperServiceByJsonRPC, CLAccountHash } = require("casper-js-sdk");
+const { RuntimeArgs, CLValueBuilder, Contracts, CasperClient, Keys, CLPublicKey, CLURef, Signer, CasperServiceByJsonRPC, CLAccountHash, CLByteArray } = require("casper-js-sdk");
 const { BN } = require("bn.js");
 const client = new CasperClient("https://rpc.testnet.casperlabs.io/rpc");
 
-const vestingWasm = new Uint8Array(fs.readFileSync("vesting.wasm"));
+const vestingWasm = new Uint8Array(fs.readFileSync("contract.wasm"));
 const depositWasm = new Uint8Array(fs.readFileSync("deposit.wasm"));
 
 const keys = Keys.Ed25519.loadKeyPairFromPrivateFile("test.pem");
 
+const token = "6e7b9e79c3a3907f5befb4079169d2e969dcb36a3a646eb0b2418cfa1826a1d7";
+
 const contract = new Contracts.Contract(client);
-contract.setContractHash("hash-78dd5f61c96bc394a1cfa5f13a3dec59fe3cd9da58fa99c67d702124c9350a6f");
+contract.setContractHash("hash-cba19cd9f0848ec807ce88e25f2e754e2fe136be63e74972367f2904cee59978");
 
 async function install() {
   const args = RuntimeArgs.fromMap({
     admin: new CLAccountHash(Buffer.from("45d00e5cb25fb65b9836806fe2a6e5633920b63a98cfbd8417ab7f887ae0b606", "hex")),
     recipient: new CLAccountHash(Buffer.from("74250dfec3c4465a196c371a8040de3e9bfddbd8bc16278919cb8008ffa1bf8d", "hex")),
-    cliff_amount: CLValueBuilder.u512(0),
-    cliff_timestamp: CLValueBuilder.u512(Date.now() + 100000000),
-    drip_duration: CLValueBuilder.u512(Date.now() + 1000),
-    drip_amount: CLValueBuilder.u512(110000000000),
-    total_amount: CLValueBuilder.u512(110000000000),
-    admin_release_duration: CLValueBuilder.u512(0),
+    cliff_amount: CLValueBuilder.u256(0),
+    cliff_timestamp: CLValueBuilder.u256(Date.now() + 100000000),
+    drip_duration: CLValueBuilder.u256(Date.now() + 1000),
+    drip_amount: CLValueBuilder.u256(10 * Math.pow(10, 8)),
+    total_amount: CLValueBuilder.u256(100 * Math.pow(10, 8)),
+    admin_release_duration: CLValueBuilder.u256(0),
   });
 
   const deploy = contract.install(vestingWasm, args, "110000000000", keys.publicKey, "casper-test", [keys]);
@@ -37,12 +39,12 @@ async function install() {
 
 async function deposit() {
   const args = RuntimeArgs.fromMap({
-    deposit_contract_hash: new CLAccountHash(Buffer.from("78dd5f61c96bc394a1cfa5f13a3dec59fe3cd9da58fa99c67d702124c9350a6f", "hex")),
-    token_contract_hash: new CLAccountHash(Buffer.from("7e4cfdd7bfae515e013209a8ff91f7bf35955e69195d41e6376058f098a18f4b", "hex")),
-    amount: CLValueBuilder.u256(11000000),
+    deposit_contract_hash: new CLAccountHash(Buffer.from("cba19cd9f0848ec807ce88e25f2e754e2fe136be63e74972367f2904cee59978", "hex")),
+    token_contract_hash: new CLAccountHash(Buffer.from(token, "hex")),
+    amount_mint: CLValueBuilder.u256(100 * Math.pow(10, 8)),
   });
 
-  const deploy = contract.install(depositWasm, args, "110000000000", keys.publicKey, "casper-test", [keys]);
+  const deploy = contract.install(depositWasm, args, 5 * Math.pow(10, 9), keys.publicKey, "casper-test", [keys]);
 
   try {
     const tx = await client.putDeploy(deploy);
@@ -101,7 +103,6 @@ const withdraw = async () => {
 // fetchVesting();
 
 // getDepositPurse();
-
 // deposit();
 
 install();
