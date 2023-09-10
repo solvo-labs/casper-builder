c134a8ba063cc4d21fe2c0df3f20859229b73fda
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-09-11 01:04:02 +0300

    nft approve fucntion

--- nft-index.js
+++ nft-index.js
@@ -1,5 +1,18 @@
 const fs = require("fs");
-const { RuntimeArgs, CLValueBuilder, Contracts, CasperClient, Keys, CLPublicKey, CLKeyParameters, Signer, CasperServiceByJsonRPC, CLTypeBuilder } = require("casper-js-sdk");
+const {
+  RuntimeArgs,
+  CLValueBuilder,
+  Contracts,
+  CasperClient,
+  Keys,
+  CLPublicKey,
+  CLKey,
+  CLByteArray,
+  CLKeyParameters,
+  Signer,
+  CasperServiceByJsonRPC,
+  CLTypeBuilder,
+} = require("casper-js-sdk");
 const { error, log } = require("console");
 
 const collectionName = "Babisko";
@@ -12,7 +25,9 @@ const keys = Keys.Ed25519.loadKeyPairFromPrivateFile("test.pem");
 
 const wasm = new Uint8Array(fs.readFileSync("contract.wasm"));
 
-const MOCKED_RECIPIENT_PUBKEY = CLPublicKey.fromHex("0129d4c70a9b7c07d9c69c08bdc632f19b6e736f3f5184e00c578259489376ed15");
+const MOCKED_RECIPIENT_PUBKEY = CLPublicKey.fromHex(
+  "0129d4c70a9b7c07d9c69c08bdc632f19b6e736f3f5184e00c578259489376ed15"
+);
 
 async function install() {
   const args = RuntimeArgs.fromMap({
@@ -38,7 +53,14 @@ async function install() {
     holder_mode: CLValueBuilder.u8(2),
   });
 
-  const deploy = contract.install(wasm, args, "300000000000", keys.publicKey, "casper-test", [keys]);
+  const deploy = contract.install(
+    wasm,
+    args,
+    "300000000000",
+    keys.publicKey,
+    "casper-test",
+    [keys]
+  );
   //   console.log(deploy);
   try {
     return await client.putDeploy(deploy);
@@ -52,7 +74,9 @@ async function install() {
 //   .catch((error) => console.error(error));
 
 const mint = async () => {
-  contract.setContractHash("hash-98a99e2f424a09fd688a8f430c779341ca6664232c50199d89548644eb27baea");
+  contract.setContractHash(
+    "hash-98a99e2f424a09fd688a8f430c779341ca6664232c50199d89548644eb27baea"
+  );
 
   const myKey = keys.publicKey;
 
@@ -69,7 +93,14 @@ const mint = async () => {
     token_meta_data: CLValueBuilder.string(JSON.stringify(inputArgs.meta)),
   });
 
-  const deploy = contract.callEntrypoint("mint", args, keys.publicKey, "casper-test", "5000000000", [keys]);
+  const deploy = contract.callEntrypoint(
+    "mint",
+    args,
+    keys.publicKey,
+    "casper-test",
+    "5000000000",
+    [keys]
+  );
   console.log(deploy);
   try {
     return await client.putDeploy(deploy);
@@ -79,7 +110,9 @@ const mint = async () => {
 };
 
 const mint2 = async () => {
-  contract.setContractHash("hash-98a99e2f424a09fd688a8f430c779341ca6664232c50199d89548644eb27baea");
+  contract.setContractHash(
+    "hash-98a99e2f424a09fd688a8f430c779341ca6664232c50199d89548644eb27baea"
+  );
 
   const myKey = keys.publicKey;
 
@@ -96,7 +129,14 @@ const mint2 = async () => {
     token_meta_data: CLValueBuilder.string(JSON.stringify(inputArgs.meta)),
   });
 
-  const deploy = contract.callEntrypoint("mint", args, keys.publicKey, "casper-test", "5000000000", [keys]);
+  const deploy = contract.callEntrypoint(
+    "mint",
+    args,
+    keys.publicKey,
+    "casper-test",
+    "5000000000",
+    [keys]
+  );
 
   try {
     return await client.putDeploy(deploy);
@@ -127,7 +167,9 @@ const mint2 = async () => {
 // };
 
 const transfer = async () => {
-  contract.setContractHash("hash-98a99e2f424a09fd688a8f430c779341ca6664232c50199d89548644eb27baea");
+  contract.setContractHash(
+    "hash-98a99e2f424a09fd688a8f430c779341ca6664232c50199d89548644eb27baea"
+  );
 
   const myKey = keys.publicKey;
 
@@ -137,7 +179,14 @@ const transfer = async () => {
     token_id: CLValueBuilder.u64(0),
   });
 
-  const deploy = contract.callEntrypoint("transfer", args, keys.publicKey, "casper-test", "11000000000", [keys]);
+  const deploy = contract.callEntrypoint(
+    "transfer",
+    args,
+    keys.publicKey,
+    "casper-test",
+    "11000000000",
+    [keys]
+  );
   console.log(deploy);
   try {
     return await client.putDeploy(deploy);
@@ -146,6 +195,46 @@ const transfer = async () => {
   }
 };
 
-mint2()
-  .then((deployHash) => console.log("https://testnet.cspr.live/deploy/" + deployHash))
+const approve = async () => {
+  contract.setContractHash(
+    "hash-b1acd833758036848ff21448621e9a545af5e96c9a6bf27a4103d59bb925867f"
+  );
+
+  const myKey = keys.publicKey;
+  console.log("keyyy", myKey.toAccountHashStr());
+
+  const args = RuntimeArgs.fromMap({
+    operator: new CLKey(
+      new CLByteArray(
+        Uint8Array.from(
+          Buffer.from(
+            "2c2778415e856d528e2717eba8578b907de8b366b7a9860a3c63063e4a9ef2a7",
+            "hex"
+          )
+        )
+      )
+    ),
+    token_id: CLValueBuilder.u64(0),
+  });
+
+  const deploy = contract.callEntrypoint(
+    "approve",
+    args,
+    keys.publicKey,
+    "casper-test",
+    "11000000000",
+    [keys]
+  );
+  console.log(deploy);
+  try {
+    return await client.putDeploy(deploy);
+  } catch (error) {
+    return error;
+  }
+};
+
+approve()
+  .then((deployHash) =>
+    console.log("https://testnet.cspr.live/deploy/" + deployHash)
+  )
   .catch((error) => console.error("error", error));
